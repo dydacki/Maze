c27e8a38370f91ffcad4516b424a5a4ce95e3d10
dydacki.wojciech@gmail.com <dydacki.wojciech@gmail.com>
2021-07-07 14:04:37 +0200

    Added VerySimpleMazeFactoryFixture class.

--- AtlasCopco.Maze.VerySimpleMaze.Test/VerySimpleMazeFactoryFixture.cs
+++ AtlasCopco.Maze.VerySimpleMaze.Test/VerySimpleMazeFactoryFixture.cs
@@ -1,11 +1,30 @@
 ﻿namespace AtlasCopco.Maze.VerySimpleMaze.Test
 {
-    using AtlasCopco.Maze.Core;
+    using System;
+
     using AtlasCopco.Maze.VerySimpleMaze;
     using NUnit.Framework;
 
     [TestFixture]
     public class VerySimpleMazeFactoryFixture
     {
+        [Test]
+        public void ShouldBuildSquareBasedMazeWithEntranceOnTheEdge() 
+        {
+            var mazeSize = new Random().Next(4, 8);
+            var verySimpleMaze = new VerySimpleMazeFactory().BuildMaze(mazeSize);
+
+            Assert.That(verySimpleMaze.Length, Is.EqualTo(mazeSize));
+            Assert.That(verySimpleMaze.Width, Is.EqualTo(mazeSize));
+
+            var entLocation = verySimpleMaze.EntranceLocation;
+            Assert.True(entLocation.X == 0 || entLocation.Y == 0);
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenAttemptedToConstructTooSmallMaze() 
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new VerySimpleMazeFactory().BuildMaze(2));
+        }
     }
 }
